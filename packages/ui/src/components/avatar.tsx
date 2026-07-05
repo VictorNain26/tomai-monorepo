f03fc4844ee708e0d783a9907f1043b613b50257
Victor Lenain <victor.lenain26@gmail.com>
2026-07-05 22:36:33 +0100

    feat(mobile): avatar size parity and skeleton on motion tokens

--- packages/ui/src/components/avatar.tsx
+++ packages/ui/src/components/avatar.tsx
@@ -1,19 +1,29 @@
 import * as React from "react";
 import * as AvatarPrimitive from "@radix-ui/react-avatar";
+import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "../lib/utils";
+
+const avatarVariants = cva("relative flex shrink-0 overflow-hidden rounded-full", {
+  variants: {
+    size: {
+      sm: "h-8 w-8",
+      md: "h-10 w-10",
+      lg: "h-12 w-12",
+      xl: "h-16 w-16",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+  },
+});
 
 function Avatar({
   className,
+  size,
   ...props
-}: React.ComponentProps<typeof AvatarPrimitive.Root>) {
+}: React.ComponentProps<typeof AvatarPrimitive.Root> & VariantProps<typeof avatarVariants>) {
   return (
-    <AvatarPrimitive.Root
-      className={cn(
-        "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
-        className
-      )}
-      {...props}
-    />
+    <AvatarPrimitive.Root className={cn(avatarVariants({ size }), className)} {...props} />
   );
 }
 
